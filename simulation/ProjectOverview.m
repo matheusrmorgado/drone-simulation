88916ce5b4eea84f97450837164869e1f6fce3d2
Matheus Morgado <matheus.ramos.morgado@gmail.com>
2017-12-30 10:54:31 -0200

    Add files to repository

---
+++ simulation/ProjectOverview.m
@@ -0,0 +1,5 @@
+% Drone Simulation Analysys based on the project from Khanh Dang
+
+% Requires: Simscape Multibody, Simulink, MATLAB
+
+% More details at http://www.mathworks.com/matlabcentral/fileexchange/48052-simulate-quadrotor-in-simulink-with-simmechanics
